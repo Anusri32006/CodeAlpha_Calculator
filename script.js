c4e677ed1b673a3f11d9113fe2890c9b5b2dd4d8
Anusri32006 <anusri32006@gmail.com>
2026-05-20 16:37:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const display = document.getElementById('display');
+
+function append(value){
+
+    display.value += value;
+
+}
+
+function clearDisplay(){
+
+    display.value = '';
+
+}
+
+function backspace(){
+
+    display.value = display.value.slice(0,-1);
+
+}
+
+function calculate(){
+
+    try{
+
+        display.value = eval(display.value);
+
+    }
+    catch{
+
+        display.value = 'Error';
+
+    }
+
+}
+
+document.addEventListener('keydown',(e)=>{
+
+    if(!isNaN(e.key) || ['+','-','*','/','.'].includes(e.key)){
+        append(e.key);
+    }
+
+    if(e.key === 'Enter'){
+        calculate();
+    }
+
+    if(e.key === 'Backspace'){
+        backspace();
+    }
+
+});
